4a5e4e7d7c02b8661a32250d56c3cf9e8ed242cc
Manuel Leung Wu <manuelleung93@gmail.com>
2014-11-17 16:18:50 -0500

    character added to tiled map

--- Emp-core/src/edu/emp/game/EmpGame.java
+++ Emp-core/src/edu/emp/game/EmpGame.java
@@ -2,19 +2,34 @@ package edu.emp.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile;
+import com.badlogic.gdx.math.Vector3;
 
 // This is the game source 
-public class EmpGame extends ApplicationAdapter {
+public class EmpGame extends ApplicationAdapter implements InputProcessor {
 	// Variables for the map, camera, renderer
 	TiledMap tileMap;
 	OrthographicCamera camera;
 	TiledMapRenderer tiledMapRenderer;
+	
+	//character
+	Texture texture;
+	
+	//sprites
+	SpriteBatch batch;
+	Sprite sprite;
 	
 	@Override
 	public void create () {
@@ -24,8 +39,16 @@ public class EmpGame extends ApplicationAdapter {
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, w, h);
 		// Load our tile map into our renderer
-		tileMap = new TmxMapLoader().load("test.tmx");
+		tileMap = new TmxMapLoader().load("test2.tmx");
 		tiledMapRenderer = new OrthogonalTiledMapRenderer(tileMap);	
+		Gdx.input.setInputProcessor(this);
+		
+		batch = new SpriteBatch();
+		texture = new Texture(Gdx.files.internal("image.png"));
+		sprite = new Sprite(texture);
+		
+
+		
 	}
 
 	@Override
@@ -38,5 +61,84 @@ public class EmpGame extends ApplicationAdapter {
 		// Set the viewpoint from camera and render map
 		tiledMapRenderer.setView(camera);
 		tiledMapRenderer.render();
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+		sprite.draw(batch);
+		batch.end();
 	}
+	
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		Vector3 clickCoordinates = new Vector3(screenX, screenY, 0);
+		Vector3 position = camera.unproject(clickCoordinates);
+		sprite.setPosition(position.x, position.y);
+		return true;
+	}
+
+	@Override
+	public boolean keyDown(int arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int arg0, int arg1, int arg2) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int arg0, int arg1, int arg2, int arg3) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
